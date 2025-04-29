a9e052f2587249f8145c994e81ea1b6b6ee463e1
Mert <mertakdemir9399@gmail.com>
2025-04-29 16:05:11 +0200

    Gürtelfarben den Graduiereungen hinzufgefügt

--- ShindokanApp/Views/Klassen/Student.swift
+++ ShindokanApp/Views/Klassen/Student.swift
@@ -44,18 +44,58 @@ enum Course: String, Codable, CaseIterable {
 
 
 enum BeltGrade: String, Codable, CaseIterable {
-    case kyu9 = "9. Kyu"
-    case kyu89 = "9./8. Kyu"
-    case kyu8 = "8. Kyu"
-    case kyu78 = "8./7. Kyu"
-    case kyu7 = "7. Kyu"
-    case kyu67 = "7./6. Kyu"
-    case kyu6 = "6. Kyu"
-    case kyu56 = "6./5. Kyu"
-    case kyu5 = "5. Kyu"
-    case kyu45 = "5./4. Kyu"
-    case kyu4 = "4. Kyu"
-    case kyu3 = "3. Kyu"
-    case kyu2 = "2. Kyu"
-    case kyu1 = "1. Kyu"
+    case kyu9 = "9. Kyu - weiß"
+    case kyu89 = "9./8. Kyu - weiß/gelb"
+    case kyu8 = "8. Kyu - gelb"
+    case kyu78 = "8./7. Kyu - gelb/orange"
+    case kyu7 = "7. Kyu - orange"
+    case kyu67 = "7./6. Kyu - orange-grün"
+    case kyu6 = "6. Kyu - grün"
+    case kyu56 = "6./5. Kyu - grün/blau"
+    case kyu5 = "5. Kyu - blau"
+    case kyu4 = "4. Kyu - blau 2"
+    case kyu3 = "3. Kyu - braun 1"
+    case kyu2 = "2. Kyu - braun 2"
+    case kyu1 = "1. Kyu - braun 3"
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(String.self)
+        
+        if let grade = BeltGrade(rawValue: rawValue) {
+            self = grade
+        } else {
+            // Versuche alternative Zuordnungen
+            switch rawValue {
+            case "9. Kyu":
+                self = .kyu9
+            case "9./8. Kyu":
+                self = .kyu89
+            case "8. Kyu":
+                self = .kyu8
+            case "8./7. Kyu":
+                self = .kyu78
+            case "7. Kyu":
+                self = .kyu7
+            case "7./6. Kyu":
+                self = .kyu67
+            case "6. Kyu":
+                self = .kyu6
+            case "6./5. Kyu":
+                self = .kyu56
+            case "5. Kyu":
+                self = .kyu5
+            case "4. Kyu":
+                self = .kyu4
+            case "3. Kyu":
+                self = .kyu3
+            case "2. Kyu":
+                self = .kyu2
+            case "1. Kyu":
+                self = .kyu1
+            default:
+                throw DecodingError.dataCorruptedError(in: container, debugDescription: "Ungültiger Gürtelgrad: \(rawValue)")
+            }
+        }
+    }
 }
